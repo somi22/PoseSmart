eb25f58193bb5056df23e8664f3e4d50b6ff922a
gkuer <gkuer95@gmail.com>
2022-03-23 14:08:07 +0900

    FEAT: Signup, Signout, SetTime

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,10 +1,55 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .serializers import AccountSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from django.contrib.auth import login as auth_login
+from .models import User
 
-def accounts():
-    pass
+@api_view(['POST','DELETE'])
+def accounts(request):
+    if request.method == 'POST':
+        serializer = AccountSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-def login():
-    pass
+    elif request.method == 'DELETE':
+        serializer = AccountSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            # 나중에 get_object_or 404 변환 필요
+            if serializer.get_value('username') == request.user.email:
+                user = User.objects.get(email=serializer.get_value('username'))
+                user.delete()
 
-def time():
-    pass
+                return Response(serializer.data, status=status.HTTP_204_NO_CONTENT)
+            else:
+                data = {
+                    "errors": "정상적인 삭제 요청이 아닙니다.(사용자 불일치)"
+                }
+                return Response(data, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['POST'])
+def login(request):
+    if request.method == 'POST':
+        serializer = AccountSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            auth_login(request, serializer.get_user)
+
+@api_view(['GET', 'PUT'])
+def time(request):
+    user = User.objects.get(pk=request.user.pk)
+    if request.method == 'GET':
+        blank_time, neck_time, stretching_time = user.blink_time, user.neck_time, user.stretching_time
+        data = {
+            'blank_time': blank_time,
+            'neck_time': neck_time,
+            'stretching_time': stretching_time
+        }
+        return Response(data, status=status.HTTP_200_OK)
+
+    elif request.method == 'PUT':
+        serializer = AccountSerializer(instance=user, data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
