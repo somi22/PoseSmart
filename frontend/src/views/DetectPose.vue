9d20288649ffb96e51115c1fd6c8ed4c0724aa48
박다원 <dawon950@gmail.com>
2022-04-01 13:15:46 +0900

    Chore : 필요없는 주석문 삭제

--- frontend/src/views/DetectPose.vue
+++ frontend/src/views/DetectPose.vue
@@ -197,41 +197,10 @@ export default Vue.extend({
               console.log(error);
             }
           };
-
           reader.readAsDataURL(data1);
         });
     },
-    // blobToString(b) {
-    //     var u, x;
-    //     u = URL.createObjectURL(b);
-    //     x = new XMLHttpRequest();
-    //     x.open("GET", u, false); // although sync, you're not fetching over internet
-    //     x.send();
-    //     URL.revokeObjectURL(u);
-    //     return x.responseText;
-    // },
-
-    downloadFiles(data, file_name, file_type) {
-      var file = new Blob([data], { type: file_type });
-      //   if (window.navigator.msSaveOrOpenBlob)
-      //     window.navigator.msSaveOrOpenBlob(file, file_name);
-      //   else {
-      //     var a = document.createElement("a"),
-      //       url = URL.createObjectURL(file);
-      //     a.href = url;
-      //     a.download = file_name;
-      //     document.body.appendChild(a);
-      //     a.click();
-      //     setTimeout(function () {
-      //       document.body.removeChild(a);
-      //       window.URL.revokeObjectURL(url);
-      //     }, 0);
-      //   }
-    },
   },
-  // mounted() {
-  //   this.init();
-  // },
 });
 </script>
 
